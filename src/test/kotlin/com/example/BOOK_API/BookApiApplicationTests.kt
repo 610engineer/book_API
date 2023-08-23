8d8f7d9bac8084e6859bc6973b49aaa597a4a8b8
610engineer <m_muto@jour-1.com>
2023-08-24 00:40:21 +0900

    first commit

---
+++ src/test/kotlin/com/example/BOOK_API/BookApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.BOOK_API
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
